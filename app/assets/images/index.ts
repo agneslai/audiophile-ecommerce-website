4ec3df672918e8bdfd8dfbeae180d9923d78ab1f
Agnes Lai <agnes.dev@pm.me>
2024-02-26 18:56:36 +0800

    feat: update section product style with pattern background

--- app/assets/images/index.ts
+++ app/assets/images/index.ts
@@ -1,6 +1,7 @@
 import Logo from './logo.svg';
 import IconArrowRight from './icon-arrow-right.svg';
 import IconCart from './icon-cart.svg';
+import PatternCirle from './pattern-circles.svg';
 import ThumbnailEarphones from './image-category-thumbnail-earphones.png';
 import ThumbnailHeadphones from './image-category-thumbnail-headphones.png';
 import ThumbnailSpeakers from './image-category-thumbnail-speakers.png';
@@ -10,6 +11,7 @@ export {
   Logo,
   IconArrowRight,
   IconCart,
+  PatternCirle,
   ThumbnailEarphones,
   ThumbnailHeadphones,
   ThumbnailSpeakers,
--- app/sections/SectionProducts/index.tsx
+++ app/sections/SectionProducts/index.tsx
@@ -2,18 +2,27 @@ import Image from 'next/image';
 import styles from './index.module.scss';
 import Container from '@/app/components/Container';
 import Button from '@/app/components/Button';
-import { ImageSpeakerZx9 } from '@/app/assets/images';
+import { ImageSpeakerZx9, PatternCirle } from '@/app/assets/images';
 
 const SectionProducts = () => {
   return (
     <section className={styles.sectionProducts}>
       <Container>
-        <div className={styles.sectionProducts__wrapper}>
-          <div className={styles.sectionProducts__zx9Image}>
-
+        <div className={styles.sectionProducts__speaker}>
+          <div className={styles['sectionProducts__speaker--imageWrapper']}>
+            <Image
+              src={PatternCirle}
+              alt='circle pattern background'
+              className={styles['sectionProducts__speaker--patternBg']}
+            />
+            <Image
+              src={ImageSpeakerZx9}
+              alt="Speaker"
+              className={styles['sectionProducts__speaker--image']}
+            />
           </div>
 
-          <div className={styles.sectionProducts__zx9}>
+          <div className={styles['sectionProducts__speaker--text']}>
             <h1>ZX9<br />SPEAKER</h1>
             <p>Upgrade to premium speakers that are<br /> phenomenally built to deliver truly remarkable sound.</p>
             <Button variant='black'>SEE PRODUCT</Button>
@@ -25,4 +34,4 @@ const SectionProducts = () => {
   )
 }
 
-export default SectionProducts
+export default SectionProducts;
