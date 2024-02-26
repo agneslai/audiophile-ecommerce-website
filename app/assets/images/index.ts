06fe6145892b31a6705c4e24172085acda162eef
Agnes Lai <agnes.dev@pm.me>
2024-02-26 19:27:43 +0800

    feat: completed section products

--- app/assets/images/index.ts
+++ app/assets/images/index.ts
@@ -2,10 +2,15 @@ import Logo from './logo.svg';
 import IconArrowRight from './icon-arrow-right.svg';
 import IconCart from './icon-cart.svg';
 import PatternCirle from './pattern-circles.svg';
+
 import ThumbnailEarphones from './image-category-thumbnail-earphones.png';
 import ThumbnailHeadphones from './image-category-thumbnail-headphones.png';
 import ThumbnailSpeakers from './image-category-thumbnail-speakers.png';
+
+// Home Page
+import ImageSpeakerZx7 from './image-speaker-zx7.jpg';
 import ImageSpeakerZx9 from './image-speaker-zx9.png';
+import ImageEarphoneYx1 from './image-earphones-yx1.jpg';
 
 export {
   Logo,
@@ -15,5 +20,7 @@ export {
   ThumbnailEarphones,
   ThumbnailHeadphones,
   ThumbnailSpeakers,
+  ImageSpeakerZx7,
   ImageSpeakerZx9,
+  ImageEarphoneYx1
 };
--- app/sections/SectionProducts/index.tsx
+++ app/sections/SectionProducts/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import styles from './index.module.scss';
 import Container from '@/app/components/Container';
 import Button from '@/app/components/Button';
-import { ImageSpeakerZx9, PatternCirle } from '@/app/assets/images';
+import { ImageEarphoneYx1, ImageSpeakerZx9, PatternCirle } from '@/app/assets/images';
 
 const SectionProducts = () => {
   return (
@@ -26,6 +26,22 @@ const SectionProducts = () => {
             <h1>ZX9<br />SPEAKER</h1>
             <p>Upgrade to premium speakers that are<br /> phenomenally built to deliver truly remarkable sound.</p>
             <Button variant='black'>SEE PRODUCT</Button>
+          </div>
+        </div>
+
+        <div className={styles.sectionProducts__speakerZx7}>
+          <p>ZX7 Speaker</p>
+          <Button variant='secondary'>See Product</Button>
+        </div>
+
+        <div className={styles.sectionProducts__earphone}>
+            <Image src={ImageEarphoneYx1} alt='earphone YX1' className={styles['sectionProducts__earphone--image']} />
+
+          <div className={styles['sectionProducts__earphone--textWrapper']}>
+            <div>
+              <p>YX1 EARPHONES</p>
+              <Button variant='secondary'>See Product</Button>
+            </div>
           </div>
         </div>
       </Container>
